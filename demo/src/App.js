33c5958194bc9d6c70ebc021db9462de4fb7636c
Michael Tu <tuzhucheng@outlook.com>
2018-05-27 21:20:30 -0400

    Add SM-CNN section

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Jumbotron, Button } from 'reactstrap';
+import { Button, Container, InputGroup, InputGroupText, Input, Label, Jumbotron } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +15,31 @@ class App extends Component {
             </p>
           </Container>
         </Jumbotron>
+        <div className="pt-3 pb-5 px-3">
+          <Container>
+            <div>
+              <h2 class="display-4">SM-CNN</h2>
+              <p class="lead">Short text pair ranking</p>
+              <InputGroup>
+                <Input placeholder="how are glacier caves formed" />
+              </InputGroup>
+              <br />
+              <InputGroup>
+                <Input placeholder="the ice facade is approximately 60 m high" />
+              </InputGroup>
+              <br />
+              <Button className="float-right" color="secondary">Evaluate</Button>
+            </div>
+          </Container>
+        </div>
+        <div className="bg-light pt-3 pb-5 px-3">
+          <Container>
+            <div>
+              <h2 class="display-4">Kim CNN</h2>
+              <p class="lead">Sentence classification</p>
+            </div>
+          </Container>
+        </div>
       </div>
     );
   }
